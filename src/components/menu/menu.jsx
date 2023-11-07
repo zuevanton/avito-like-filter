f4717f903001c190d66846c6724939f82b9bf39d
Anton Zuev <wnfgnqpw@gmail.com>
2023-11-07 16:47:17 +0300

    add getAllParents func

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,4 +1,5 @@
 import { getAlldescendants } from "../../utils/getAlldescendants"
+import { getAllParents } from "../../utils/getAllParents"
 import { SubMenu } from "../subMenu/subMenu"
 
 export const Menu = ({categories, selected, setSelected}) => {
@@ -6,9 +7,9 @@ export const Menu = ({categories, selected, setSelected}) => {
 
   const onCheckboxChange = id => {
     setSelected(prev => {
+      console.log(getAllParents(id, categories, selected))
       if(prev.includes(id)){
         const allDescendants = getAlldescendants(id, categories)
-        
         return prev.filter(prev => !allDescendants.includes(prev) && prev !== id)
       }
       return Array.from(new Set([...prev, id, ...getAlldescendants(id, categories)]))
--- src/utils/getAllParents.js
+++ src/utils/getAllParents.js
@@ -8,3 +8,17 @@
 // 
 //        ✓F
 //    ✓M
+export const getAllParents = (id, categories, selected, result = []) => {
+  const parentId = categories.filter(item => item.id === id)[0].parentId
+  const isEveryChildSelected = categories
+    .filter(item => item.parentId === id)
+    .every(item => selected.includes(item.id))
+  // console.log(categories.filter(item => item.parentId === id))
+  // console.log(isEveryChildSelected)
+  //!selected.includes(parentId)
+  if(parentId === undefined || !isEveryChildSelected) {
+    return result
+  }
+
+  return getAllParents(parentId, categories, selected, [...result, parentId])
+}
